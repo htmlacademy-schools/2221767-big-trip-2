0e7850967983e833ede343f466bd9b341328749f
Mapkycc <you@example.com>
2023-04-19 00:32:59 +0500

    Добавил структуры данных

---
+++ src/view/trip-list.js
@@ -0,0 +1,25 @@
+import { createElement} from '../render';
+
+const createTripListTemplate = () => (
+  `<ul class="trip-events__list">
+  </ul>`
+);
+
+export default class TripList {
+  #element = null;
+  get template() {
+    return createTripListTemplate();
+  }
+
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}

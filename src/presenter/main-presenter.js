02d9abb95e68f63efd4d25ad728cc1fa35acf53b
Mapkycc <you@example.com>
2023-05-25 03:49:08 +0500

    5.8. Большие перемены (часть 1)

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -2,7 +2,7 @@ import { render, RenderPosition } from '../framework/render';
 import Sort from '../view/sort';
 import TripList from '../view/trip-list';
 import NoPoint from '../view/no-point';
-import PointPresenter from "./point-presenter";
+import PointPresenter from '../presenter/point-presenter';
 import { updateItem } from '../utils';
 
 export default class MainPresenter {
@@ -27,38 +27,38 @@ export default class MainPresenter {
     if (this.#tripPoints.length === 0) {
       return this.#renderNoPoints();
     }
-      this.#renderSort();
-      this.#renderPointsList();
-    }
+    this.#renderSort();
+    this.#renderPointsList();
+  }
 
   #renderSort = () => {
     render(this.#sort, this.#container, RenderPosition.AFTERBEGIN);
-  }
+  };
 
   #renderNoPoints = () => {
     render(this.#noPoint, this.#container, RenderPosition.AFTERBEGIN);
-  }
+  };
 
   #renderPoints = (from, to) => {
     this.#tripPoints
       .slice(from, to)
       .forEach((point) => this.#renderPoint(point));
-  }
+  };
 
   #renderPointsList = () => {
     render(this.#pointsList, this.#container);
     this.#renderPoints(0, this.#tripPoints.length);
-  }
+  };
 
   #renderPoint = (point) => {
     const pointPresenter = new PointPresenter(this.#pointsList.element, this.#handlePointChange, this.#handleModeChange);
     pointPresenter.init(point);
     this.#pointPresenter.set(point.id, pointPresenter);
-  }
+  };
 
   //#clearEventsList = () => {
-    //this.#pointPresenter.forEach((presenter) => presenter.destroy());
-    //this.#pointPresenter.clear();
+  //this.#pointPresenter.forEach((presenter) => presenter.destroy());
+  //this.#pointPresenter.clear();
   //};
 
   #handlePointChange = (updatedPoint) => {

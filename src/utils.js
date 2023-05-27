b981e860d7f6c6388351a299e3aa0cc5e4534ed7
Mapkycc <you@example.com>
2023-05-27 18:14:33 +0500

    4.17. Шаблонизируй то

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+import {FILTER_TYPE} from './mock/points';
+import dayjs from 'dayjs';
+
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
@@ -18,4 +21,14 @@ const updateItem = (items, update) => {
 
 const isEscKeyDown = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 
-export { getRandomNumber, getRandomArrayElement, updateItem, isEscKeyDown };
+const isPointPast = (pointDate) => dayjs(pointDate.dateFrom).isBefore(dayjs());
+const isPointFuture = (pointDate) => dayjs(pointDate.dateTo).isAfter(dayjs());
+
+const filter = {
+  [FILTER_TYPE.EVERYTHING]: (points) => points,
+  [FILTER_TYPE.FUTURE]: (points) => points.filter((point) => isPointFuture(point)),
+  [FILTER_TYPE.PAST]: (points) => points.filter((point) => isPointPast(point))
+};
+
+
+export { getRandomNumber, getRandomArrayElement, updateItem, isEscKeyDown, filter };
